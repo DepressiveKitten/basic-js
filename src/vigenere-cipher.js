cb47dfc59c4dcbb0a65afe8c7aba10c2e2b78f75
Дима Лопатин <mityalopatin12321@gmail.com>
2023-12-23 21:58:12 +0300

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  reverse;
+  constructor(reverse = true)
+  {
+    this.reverse = !reverse;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    return this.general(message, key, 1);
+  }
+  decrypt(message, key) {
+    return this.general(message, key, -1);
+  }
+
+  general(message, key, direction)
+  {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let answer = "";
+
+    for(let i = 0, j = 0;i<message.length;i++,j=(j+1)%key.length)
+    {
+      if(this.alphabet.indexOf(message[i])<0)
+      {
+        answer+=message[i];
+        j--;
+        continue;
+      }
+      let shift = (this.alphabet.indexOf(key[j]))*direction;
+      let char_number = this.alphabet.indexOf(message[i]);
+      answer+=this.alphabet[(char_number+shift+this.alphabet.length)%this.alphabet.length];
+    }
+
+    if(this.reverse)
+    {
+      answer = answer.split("").reverse().join("");
+    }
+
+    return answer;
   }
 }
 
